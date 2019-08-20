225b629ad06338a5ba128320a6085e581cdbb0c0
milyzc <milyzc@gmail.com>
2019-08-20 13:56:58 -0300

    Update ConsultarPeliculas.cs

--- PeliculasDeAlquiler/ConsultarPeliculas.cs
+++ PeliculasDeAlquiler/ConsultarPeliculas.cs
@@ -27,7 +27,7 @@ namespace PeliculasDeAlquiler
 
         private void ActualizarPeliculas()
         {
-            // push desde aula 7
+            // push desde aula 7 genero conflicto
             DgvPeliculas.Rows.Clear();
             var peliculas = _peliculasRepositorio.ObtenerPeliculasDT().Rows;
             var filas = new List<DataGridViewRow>();
