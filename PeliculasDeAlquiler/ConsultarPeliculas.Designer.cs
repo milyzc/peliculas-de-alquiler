c9e73e3d3aa1c67fe8d8eca5f75227cc50c9ba79
Jenifer Lombo <jenilombo@hotmail.com>
2019-08-23 11:14:46 -0300

    agregacion de grilla de genero

--- PeliculasDeAlquiler/ConsultarPeliculas.Designer.cs
+++ PeliculasDeAlquiler/ConsultarPeliculas.Designer.cs
@@ -30,12 +30,14 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConsultarPeliculas));
             this.DgvPeliculas = new System.Windows.Forms.DataGridView();
-            this.BtnRefresh = new System.Windows.Forms.Button();
             this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Titulo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.FechaLanzamiento = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Genero = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Director = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BtnRefresh = new System.Windows.Forms.Button();
+            this.comboFiltro = new System.Windows.Forms.ComboBox();
+            this.buttonBuscar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.DgvPeliculas)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,28 +52,13 @@
             this.FechaLanzamiento,
             this.Genero,
             this.Director});
-            this.DgvPeliculas.Location = new System.Drawing.Point(55, 303);
+            this.DgvPeliculas.Location = new System.Drawing.Point(41, 246);
+            this.DgvPeliculas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.DgvPeliculas.Name = "DgvPeliculas";
             this.DgvPeliculas.ReadOnly = true;
             this.DgvPeliculas.RowTemplate.Height = 24;
-            this.DgvPeliculas.Size = new System.Drawing.Size(964, 368);
+            this.DgvPeliculas.Size = new System.Drawing.Size(723, 299);
             this.DgvPeliculas.TabIndex = 0;
-            // 
-            // BtnRefresh
-            // 
-            this.BtnRefresh.BackColor = System.Drawing.Color.Transparent;
-            this.BtnRefresh.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnRefresh.BackgroundImage")));
-            this.BtnRefresh.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.BtnRefresh.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.BtnRefresh.FlatAppearance.BorderColor = System.Drawing.Color.Bisque;
-            this.BtnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnRefresh.ForeColor = System.Drawing.Color.Transparent;
-            this.BtnRefresh.Location = new System.Drawing.Point(953, 234);
-            this.BtnRefresh.Name = "BtnRefresh";
-            this.BtnRefresh.Size = new System.Drawing.Size(66, 63);
-            this.BtnRefresh.TabIndex = 1;
-            this.BtnRefresh.UseVisualStyleBackColor = false;
-            this.BtnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
             // 
             // Codigo
             // 
@@ -104,14 +91,53 @@
             this.Director.Name = "Director";
             this.Director.ReadOnly = true;
             // 
+            // BtnRefresh
+            // 
+            this.BtnRefresh.BackColor = System.Drawing.Color.Transparent;
+            this.BtnRefresh.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnRefresh.BackgroundImage")));
+            this.BtnRefresh.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnRefresh.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnRefresh.FlatAppearance.BorderColor = System.Drawing.Color.Bisque;
+            this.BtnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnRefresh.ForeColor = System.Drawing.Color.Transparent;
+            this.BtnRefresh.Location = new System.Drawing.Point(715, 190);
+            this.BtnRefresh.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtnRefresh.Name = "BtnRefresh";
+            this.BtnRefresh.Size = new System.Drawing.Size(50, 51);
+            this.BtnRefresh.TabIndex = 1;
+            this.BtnRefresh.UseVisualStyleBackColor = false;
+            this.BtnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
+            // 
+            // comboFiltro
+            // 
+            this.comboFiltro.FormattingEnabled = true;
+            this.comboFiltro.Location = new System.Drawing.Point(46, 41);
+            this.comboFiltro.Name = "comboFiltro";
+            this.comboFiltro.Size = new System.Drawing.Size(139, 21);
+            this.comboFiltro.TabIndex = 2;
+            this.comboFiltro.SelectedIndexChanged += new System.EventHandler(this.comboFiltro_SelectedIndexChanged);
+            // 
+            // buttonBuscar
+            // 
+            this.buttonBuscar.Location = new System.Drawing.Point(202, 42);
+            this.buttonBuscar.Name = "buttonBuscar";
+            this.buttonBuscar.Size = new System.Drawing.Size(74, 19);
+            this.buttonBuscar.TabIndex = 3;
+            this.buttonBuscar.Text = "Buscar";
+            this.buttonBuscar.UseVisualStyleBackColor = true;
+            this.buttonBuscar.Click += new System.EventHandler(this.buttonBuscar_Click);
+            // 
             // ConsultarPeliculas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Bisque;
-            this.ClientSize = new System.Drawing.Size(1097, 701);
+            this.ClientSize = new System.Drawing.Size(823, 570);
+            this.Controls.Add(this.buttonBuscar);
+            this.Controls.Add(this.comboFiltro);
             this.Controls.Add(this.BtnRefresh);
             this.Controls.Add(this.DgvPeliculas);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ConsultarPeliculas";
             this.Text = "Películas";
             this.Load += new System.EventHandler(this.ConsultarPeliculas_Load);
@@ -129,6 +155,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn FechaLanzamiento;
         private System.Windows.Forms.DataGridViewTextBoxColumn Genero;
         private System.Windows.Forms.DataGridViewTextBoxColumn Director;
+        private System.Windows.Forms.ComboBox comboFiltro;
+        private System.Windows.Forms.Button buttonBuscar;
     }
 }
 
--- PeliculasDeAlquiler/ConsultarPeliculas.cs
+++ PeliculasDeAlquiler/ConsultarPeliculas.cs
@@ -18,6 +18,10 @@ namespace PeliculasDeAlquiler
         private void ConsultarPeliculas_Load(object sender, EventArgs e)
         {
             ActualizarPeliculas();
+            var generos = _peliculasRepositorio.ObtenerGenerosDT();
+            comboFiltro.ValueMember = "Id";
+            comboFiltro.DisplayMember = "Tipo";
+            comboFiltro.DataSource = generos;
         }
 
         private void BtnRefresh_Click(object sender, EventArgs e)
@@ -49,6 +53,32 @@ namespace PeliculasDeAlquiler
             //DgvPeliculas.Columns[1].DisplayIndex = 1;
             //DgvPeliculas.Columns[2].DisplayIndex = 2;
             //DgvPeliculas.Update();
+        }
+
+        private void comboFiltro_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonBuscar_Click(object sender, EventArgs e)
+        {
+            DgvPeliculas.Rows.Clear();
+            var valor = comboFiltro.SelectedValue;
+            var peliculas = _peliculasRepositorio.ObtenerPeliculasDTFiltros(valor.ToString()).Rows;
+            var filas = new List<DataGridViewRow>();
+            foreach (DataRow pelicula in peliculas)
+            {
+                if (pelicula.HasErrors)
+                    continue;
+                var fila = new string[] {
+                    pelicula.ItemArray[0].ToString(),
+                    pelicula.ItemArray[1].ToString(),
+                    pelicula.ItemArray[2].ToString(),
+                    pelicula.ItemArray[3].ToString(),
+                };
+                DgvPeliculas.Rows.Add(fila);
+            }
+         
         }
     }
 }
