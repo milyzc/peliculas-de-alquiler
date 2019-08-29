5e7828fc5d22aee430fe43b1ea23b6f5cd8ddc9a
Jenifer Lombo <jenilombo@hotmail.com>
2019-08-29 10:23:31 -0300

    opcion insertar

---
+++ PeliculasDeAlquiler/Modulos/Directores/NuevoDirector.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace PeliculasDeAlquiler.Modulos.Directores
+{
+    partial class NuevoDirector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelNombre = new System.Windows.Forms.Label();
+            this.labelNacionalidad = new System.Windows.Forms.Label();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.textBoxNacionalidad = new System.Windows.Forms.TextBox();
+            this.labelFecha = new System.Windows.Forms.Label();
+            this.dateTime = new System.Windows.Forms.DateTimePicker();
+            this.botonGuardar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelNombre
+            // 
+            this.labelNombre.AutoSize = true;
+            this.labelNombre.Location = new System.Drawing.Point(34, 70);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(44, 13);
+            this.labelNombre.TabIndex = 0;
+            this.labelNombre.Text = "Nombre";
+            // 
+            // labelNacionalidad
+            // 
+            this.labelNacionalidad.AutoSize = true;
+            this.labelNacionalidad.Location = new System.Drawing.Point(34, 129);
+            this.labelNacionalidad.Name = "labelNacionalidad";
+            this.labelNacionalidad.Size = new System.Drawing.Size(69, 13);
+            this.labelNacionalidad.TabIndex = 1;
+            this.labelNacionalidad.Text = "Nacionalidad";
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(84, 67);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(181, 20);
+            this.textBoxNombre.TabIndex = 2;
+            this.textBoxNombre.TextChanged += new System.EventHandler(this.textBoxNombre_TextChanged);
+            // 
+            // textBoxNacionalidad
+            // 
+            this.textBoxNacionalidad.Location = new System.Drawing.Point(109, 122);
+            this.textBoxNacionalidad.Name = "textBoxNacionalidad";
+            this.textBoxNacionalidad.Size = new System.Drawing.Size(181, 20);
+            this.textBoxNacionalidad.TabIndex = 3;
+            // 
+            // labelFecha
+            // 
+            this.labelFecha.AutoSize = true;
+            this.labelFecha.Location = new System.Drawing.Point(34, 183);
+            this.labelFecha.Name = "labelFecha";
+            this.labelFecha.Size = new System.Drawing.Size(37, 13);
+            this.labelFecha.TabIndex = 4;
+            this.labelFecha.Text = "Fecha";
+            // 
+            // dateTime
+            // 
+            this.dateTime.Location = new System.Drawing.Point(126, 175);
+            this.dateTime.Name = "dateTime";
+            this.dateTime.Size = new System.Drawing.Size(188, 20);
+            this.dateTime.TabIndex = 5;
+            // 
+            // botonGuardar
+            // 
+            this.botonGuardar.Location = new System.Drawing.Point(348, 264);
+            this.botonGuardar.Name = "botonGuardar";
+            this.botonGuardar.Size = new System.Drawing.Size(84, 30);
+            this.botonGuardar.TabIndex = 6;
+            this.botonGuardar.Text = "Guardar";
+            this.botonGuardar.UseVisualStyleBackColor = true;
+            this.botonGuardar.Click += new System.EventHandler(this.botonGuardar_Click);
+            // 
+            // NuevoDirector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.botonGuardar);
+            this.Controls.Add(this.dateTime);
+            this.Controls.Add(this.labelFecha);
+            this.Controls.Add(this.textBoxNacionalidad);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.labelNacionalidad);
+            this.Controls.Add(this.labelNombre);
+            this.Name = "NuevoDirector";
+            this.Text = "NuevoDirector";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelNombre;
+        private System.Windows.Forms.Label labelNacionalidad;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.TextBox textBoxNacionalidad;
+        private System.Windows.Forms.Label labelFecha;
+        private System.Windows.Forms.DateTimePicker dateTime;
+        private System.Windows.Forms.Button botonGuardar;
+    }
+}
---
+++ PeliculasDeAlquiler/Modulos/Directores/NuevoDirector.cs
@@ -0,0 +1,69 @@
+﻿using PeliculasDeAlquiler.Modelos;
+using PeliculasDeAlquiler.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PeliculasDeAlquiler.Modulos.Directores
+{
+    public partial class NuevoDirector : Form
+    {
+        DirectoresRepositorio directoresRepositorio;
+
+        public NuevoDirector()
+        {
+            InitializeComponent();
+            directoresRepositorio = new DirectoresRepositorio();
+
+        }
+
+        private void textBoxNombre_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void botonGuardar_Click(object sender, EventArgs e)
+        {
+            var director = new Director();
+            director.Nombre = textBoxNombre.Text;
+            director.Nacionalidad = textBoxNacionalidad.Text;
+            director.FechaNacimiento = dateTime.Value.Date;
+
+            if (!director.NombreValido())
+            {
+                MessageBox.Show("Nombre invalido");
+                return;
+
+            }
+            if (!director.ValidarNacionalidad())
+            {
+                MessageBox.Show("Nacionalidad invalida");
+                return;
+
+            }
+            if (!director.ValidarFechaNacimiento())
+            {
+                MessageBox.Show("Fecha invalida");
+                return;
+
+            }
+
+            if (directoresRepositorio.Guardar(director) )
+            {
+                MessageBox.Show("se registro con exito");
+                this.Dispose();
+            }
+
+
+        }
+
+    }
+}
+
